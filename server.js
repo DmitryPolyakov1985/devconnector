33bacbb3e43c2456085cd43cf06e48f18268989b
DmitryPolyakov1985 <dps.coding@gmail.com>
2020-09-22 15:34:51 -0600

    Before github token used

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const connectDB = require('./config/db');
+
+// connect MongoDB
+connectDB();
+
+// middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API Running");
+    res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
